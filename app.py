fa796b85eb128b4a5bc713ebfa391158cc3e996e
Mustafa Abdelhai <ele.mustafa@gmail.com>
2016-01-13 13:50:46 +0100

    fixed typos
    referrer -> referer

--- app.py
+++ app.py
@@ -26,15 +26,15 @@ GET /v1/:initials/
 GET /v1/:initials/:hex_color/
 GET /v1/:initials/:hex_color/:identifier/
 """
-svg_template = """ 
+svg_template = """
 <svg width="500" height="500" xmlns="http://www.w3.org/2000/svg">
  <g>
-  <rect id="svg_1" height="500" width="500" y="0" x="0" stroke-width="0" 
+  <rect id="svg_1" height="500" width="500" y="0" x="0" stroke-width="0"
   stroke="#000000" fill="#{}"/>
-  <text transform="matrix(1.158432126045227,0,0,1.158432126045227,-41.15026923827827,-17.281479248777032) " 
-  xml:space="preserve" text-anchor="middle" font-family="Sans-serif" font-size="150" id="svg_2" y="283" x="253" 
+  <text transform="matrix(1.158432126045227,0,0,1.158432126045227,-41.15026923827827,-17.281479248777032) "
+  xml:space="preserve" text-anchor="middle" font-family="Sans-serif" font-size="150" id="svg_2" y="283" x="253"
   stroke-linecap="null" stroke-linejoin="null" stroke-width="0" stroke="#000000" fill="#ffffff">{}</text>
-  <rect id="svg_2" height="500" width="500" y="0" x="0" stroke-width="0" 
+  <rect id="svg_2" height="500" width="500" y="0" x="0" stroke-width="0"
   stroke="#000000" fill-opacity="0.0" />
  </g>
 </svg>
@@ -64,7 +64,7 @@ def init(initials):
 @app.route('/v1/<initials>/<color>/')
 def init_color(initials, color):
     try:
-        host = domain(request.referrer)
+        host = domain(request.referer)
     except:
         host = 'unknown'
     uhid = '{}_{}_{}'.format(initials,color,host)
@@ -83,7 +83,7 @@ def init_color(initials, color):
 @app.route('/v1/<initials>/<color>/<ident>/')
 def init_color_ident(initials, color, ident):
     try:
-        host = domain(request.referrer)
+        host = domain(request.referer)
     except:
         host = 'unknown'
     uhid = '{}_{}_{}_{}'.format(initials,color,ident,host)
@@ -106,5 +106,3 @@ def init_color_ident(initials, color, ident):
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
