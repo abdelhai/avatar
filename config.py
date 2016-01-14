0f1a2f3735852bf2334739a7e355b560b381bf03
Mustafa Abdelhai <ele.mustafa@gmail.com>
2016-01-14 20:50:27 +0100

    testing

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ from mongoengine import connect
 
 def connect_db():
     connect('avatar', 
-    host='mongodb://avataru:q1w2e3r4@ds039484.mongolab.com:39484/avatar')
+    host='mongodb://avataru:<pw><host>:<port>/<dbname>')
